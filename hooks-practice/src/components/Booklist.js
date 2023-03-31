c5a1146973b17c2aa77bc686081e0b5d9406ab95
Tharindu De Costa <mtndecosta@gmail.com>
2023-03-31 18:43:07 +0530

    context consumer implemented

--- hooks-practice/src/components/Booklist.js
+++ hooks-practice/src/components/Booklist.js
@@ -1,23 +1,30 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 class Booklist extends Component {
-    static contextType = ThemeContext
-
-    render() { 
-        const {isLightTheme,light,dark} = this.context;
-        const theme = isLightTheme? light : dark;
-        return (
-            <div className="book-list" style={{color:theme.syntax,background:theme.bg}}>
-            <ul>
-                <li style={{background:theme.ui}}>Way of Kings</li>
-                <li style={{background:theme.ui}}>Fellowship of the ring</li>
-                <li style={{background:theme.ui}}>City of bones</li>
-                <li style={{background:theme.ui}}>The Final empire</li>
-            </ul>
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div
+              className="book-list"
+              style={{ color: theme.syntax, background: theme.bg }}
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>Way of Kings</li>
+                <li style={{ background: theme.ui }}>Fellowship of the ring</li>
+                <li style={{ background: theme.ui }}>City of bones</li>
+                <li style={{ background: theme.ui }}>The Final empire</li>
+              </ul>
             </div>
-        );
-    }
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
 }
- 
+
 export default Booklist;
--- hooks-practice/src/components/Navbar.js
+++ hooks-practice/src/components/Navbar.js
@@ -1,24 +1,28 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
-
-    static contextType = ThemeContext;
-    render() { 
-        const {isLightTheme,light,dark} = this.context;
-        const theme = isLightTheme? light : dark;
-        console.log(this.context);
-        return (
-            <nav style={{background:theme.ui,color:theme.syntax}}>
-                <h1>Context app</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
+  render() {
+    console.log(this.context);
+    return (
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context app</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
             </nav>
-        );
-    }
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
 }
- 
+
 export default Navbar;
