f8d84d8fed06a11daa594793fa67c76e5da189b5
Tharindu De Costa <mtndecosta@gmail.com>
2023-03-31 18:31:05 +0530

    Initial Commit

---
+++ hooks-practice/src/App.js
@@ -0,0 +1,17 @@
+import Navbar from './components/Navbar';
+import Booklist from './components/Booklist'
+import ThemeContextProvider from './contexts/ThemeContext';
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <Navbar/>
+        <Booklist/>
+      </ThemeContextProvider>
+      
+    </div>
+  );
+}
+
+export default App;
